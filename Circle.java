b115ea680d3d4d69786a36c22dee64231c55964c
OmkarGadave1996 <omkar.gadave1996@gmail.com>
2025-04-03 20:25:08 +0530

    Java Class file Updated
    There Was sum Error too

--- Circle.java
+++ Circle.java
@@ -1,16 +1,16 @@
 import java.util.*;
-class Cicle
+class Circle
 {
-	publiu static void main(String[] Args)
+	public static void main(String[] Args)
 	{
 		int r = 5,r1;
 		double area,area2;
 		Scanner sc = new Scanner(System.in);
 		area = 3.14 * r * r;
-		System.out.println("The radius of Circle is "+r+"And Area is "+area);
-		System.out.println("Enter radius of Circle = "+r1);
+		System.out.println("The radius of Circle is "+r+" And Area is "+area);
+		System.out.println("Enter radius of Circle = ");
 		r1 = sc.nextInt();
 		area2 = 3.14 * r1 * r1;
-		System.out.println("The radius of Circle is "+r1+"And Area is "+area2);
+		System.out.println("The radius of Circle is "+r1+" And Area is "+area2);
 	}
 }	
--- Rectangle.java
+++ Rectangle.java
@@ -1,18 +1,18 @@
 import java.util.*;
 class Rectangle
 {
-	publiu static void main(String[] Args)
+	public static void main(String[] Args)
 	{
 		int l = 5,b = 10, l1, b2;
 		double area,area2;
 		Scanner sc = new Scanner(System.in);
 		area = l * b;
-		System.out.println("The length of reactangle is "+l+"And Breadth is"+b+"And Area is "+area);
-		System.out.println("Enter length of rectangle = "+l1);
+		System.out.println("The length of reactangle is "+l+" And Breadth is"+b+" And Area is "+area);
+		System.out.println("Enter length of rectangle = ");
 		l1 = sc.nextInt();
-		System.out.println("Enter breadth of rectangle = "+b1);
-		b1 = sc.nextInt();
+		System.out.println("Enter breadth of rectangle = ");
+		b2 = sc.nextInt();
 		area2 = l1 *b2;
-		System.out.println("The length of reactangle is "+l1+"And Breadth is"+b1+"And Area is "+area2);
+		System.out.println("The length of reactangle is "+l1+" And Breadth is"+b2+" And Area is "+area2);
 	}
 }	
