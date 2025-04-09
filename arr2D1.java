924a7eff928352c1608e2c9d901138909e7a8c79
OmkarGadave1996 <omkar.gadave1996@gmail.com>
2025-04-09 13:07:18 +0530

    Daily Code
    Todays Work

---
+++ arr2D1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class arr2D1
+{
+	public static void main(String[] Args)
+	{
+		int a[][], i, j;
+		Scanner sc = new Scanner(System.in);
+		a = new int[2][3];
+		System.out.println("Enter the 2D array Element   ");
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				a[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("The 2D array Matrix: ");		
+		for(i=0;i<2;i++)
+		{		
+			for(j=0;j<3;j++)
+			{
+				System.out.print(+a[i][j]+"\t");
+			}
+			System.out.println("");	
+		}
+		System.out.println("Transpose of 2D Array Matrix: ");		
+		for(i=0;i<3;i++)
+		{		
+			for(j=0;j<2;j++)
+			{
+				System.out.print(+a[j][i]+"\t");
+			}
+			System.out.println("");	
+		}
+	}
+}
---
+++ arradd2D.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class arradd2D
+{
+	public static void main(String[] Args)
+	{
+		int a[][],b[][],c[][], i, j;
+		Scanner sc = new Scanner(System.in);
+		a = new int[2][3];
+		b = new int[2][3];
+		c = new int[2][3];
+		System.out.println("Enter the A's 2D array Element: ");
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				a[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Enter the B's 2D array Element: ");
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				b[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("The A's 2D array Matrix: ");		
+		for(i=0;i<2;i++)
+		{		
+			for(j=0;j<3;j++)
+			{
+				System.out.print(+a[i][j]+"\t");
+			}
+			System.out.println("");	
+		}
+		System.out.println("The B's 2D array Matrix: ");		
+		for(i=0;i<2;i++)
+		{		
+			for(j=0;j<3;j++)
+			{
+				System.out.print(+b[i][j]+"\t");
+			}
+			System.out.println("");	
+		}
+		System.out.println("The addition A & B 2D Array Matrix: ");		
+		for(i=0;i<2;i++)
+		{		
+			for(j=0;j<3;j++)
+			{
+				c[i][j] = a[i][j] + b[i][j];
+				System.out.print(+c[i][j]+"\t");
+			}
+			System.out.println("");	
+		}
+	}
+}
