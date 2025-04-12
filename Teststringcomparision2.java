8daaec6b47a55e3746370b945587c733342be0dc
OmkarGadave1996 <omkar.gadave1996@gmail.com>
2025-04-12 13:09:36 +0530

    Daily Code
    Today's Code

---
+++ Teststringcomparision2.java
@@ -0,0 +1,11 @@
+public class Teststringcomparision2
+{
+	public static void main(String ar[])
+	{
+		String s1 = "Sachin";
+		String s2 = "SACHIN";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(s2));	
+	}
+}
---
+++ Teststringcomparision4.java
@@ -0,0 +1,13 @@
+public class Teststringcomparision4
+{
+	public static void main(String ar[])
+	{
+		String s1 = "Sachin";
+		String s2 = "Sachin";
+		String s3 = "Ratan";  
+		
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s3.compareTo(s1));
+	}
+}
